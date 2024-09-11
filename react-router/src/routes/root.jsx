ad122cadb9b632ac7d603614ddfb3fd9b499b2ce
Gabriel Isaac <stewartisaac90@gmail.com>
2024-09-11 12:05:34 +0100

    manage history stack

--- react-router/src/routes/root.jsx
+++ react-router/src/routes/root.jsx
@@ -45,7 +45,9 @@ export default function Root() {
                             name="q"
                             defaultValue={q}
                             onChange={(event) => {
-                                submit(event.currentTarget.form);
+                                submit(event.currentTarget.form, {
+                                    replace: !isFirstSearch,
+                                });
                             }}
                         />
                         <div
